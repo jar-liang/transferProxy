62e2b08ac3ee24653d78f9007d609b506f685d41
JarPC <ljyan0304@163.com>
2022-06-23 00:44:26 +0800

    添加handler父类

--- common/src/main/java/me/jar/utils/TransferMsg2ByteEncoder.java
+++ common/src/main/java/me/jar/utils/TransferMsg2ByteEncoder.java
@@ -7,13 +7,14 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import me.jar.message.TransferMsg;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TransferMsg2ByteEncoder extends MessageToByteEncoder<TransferMsg> {
     @Override
-    protected void encode(ChannelHandlerContext ctx, TransferMsg msg, ByteBuf out) throws Exception {
+    protected void encode(ChannelHandlerContext ctx, TransferMsg msg, ByteBuf out) {
         int type = msg.getType().getType();
-        Map<String, Object> metaData = msg.getMetaData();
+        Map<String, Object> metaData = msg.getMetaData() != null ? msg.getMetaData() : new HashMap<>();
         byte[] metaDataBytes = JSON.toJSONBytes(metaData);
         ByteBuf byteBuf = Unpooled.copyInt(type, metaDataBytes.length);
 
